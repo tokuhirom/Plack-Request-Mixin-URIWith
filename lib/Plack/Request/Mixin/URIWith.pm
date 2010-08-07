f3e68bafd83140e6108c723cff60183c100d18ee
tokuhirom <tokuhirom@gmail.com>
2010-08-08 01:25:41 +0900

    initial import

---
+++ lib/Plack/Request/Mixin/URIWith.pm
@@ -0,0 +1,39 @@
+package Plack::Request::Mixin::URIWith;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Plack::Request::Mixin::URIWith -
+
+=head1 SYNOPSIS
+
+  use Plack::Request::Mixin::URIWith;
+
+=head1 DESCRIPTION
+
+Plack::Request::Mixin::URIWith is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
